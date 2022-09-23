5cd5b1bc2b93ff0980c7cc28c7b7a0caae1837f4
wang <401534863@qq.com>
2022-09-23 16:15:20 +0800

    mac

--- 婚姻登记/自动取消秒杀专用马凯如.py
+++ 婚姻登记/自动取消秒杀专用马凯如.py
@@ -108,15 +108,13 @@ def seckill(date, time, bianhao, dizhi, manname, manhao, phone, wumanname, wuman
     return
 
 
-def sendmail(name, key):
+def sendmail(name):
     my_sender = '401534863@qq.com'  # 发件人邮箱账号
-    my_pass = 'ezgzdnxnrpyubjii'  # 发件人邮箱密码
-    my_user = '15279101998@139.com'  # 收件人邮箱账号，我这边发送给自己
-
-    def mail():
+    my_pass = 'dvymozvmmvbmcaff'  # 发件人邮箱密码
+    def mail(my_user = '15279101998@139.com'):
         ret = True
         try:
-            msg = MIMEText(f'{name},监控报告有{key}个区有号', 'plain', 'utf-8')
+            msg = MIMEText(f'{name}', 'plain', 'utf-8')
             msg['From'] = formataddr(("秒杀监控系统", my_sender))  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
             msg['To'] = formataddr(("FK", my_user))  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
             msg['Subject'] = f"{name[:5]}有号啦"  # 邮件的主题，也可以说是标题
@@ -125,7 +123,12 @@ def sendmail(name, key):
             server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
             server.sendmail(my_sender, [my_user, ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
             server.quit()  # 关闭连接
-        except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+        except Exception as e:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+            print(e)
+            r = re.search('550', e)
+            if r is not None:
+                my_user = '401534863@qq.com'  # 收件人邮箱账号，我这边发送给自己
+                mail(my_user)
             ret = False
         return ret
 
